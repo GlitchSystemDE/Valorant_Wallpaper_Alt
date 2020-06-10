d4c8f502228d4de59cb0cac792372af3b4108022
GlitchSystemDE <GlitchSystem@outlook.de>
2020-06-10 17:06:47 +0200

    Vorschau + Fullscreen

--- app/src/main/java/com/example/valorantwallpaper/StartScreen.java
+++ app/src/main/java/com/example/valorantwallpaper/StartScreen.java
@@ -2,15 +2,36 @@ package com.example.valorantwallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class StartScreen extends AppCompatActivity {
 
+    GridView gridView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        gridView = (GridView) findViewById(R.id.grid_view);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent2 = new Intent(getApplicationContext(),VollbildActivity.class);
+                intent2.putExtra("id",position);
+                startActivity(intent2);
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/valorantwallpaper/VollbildActivity.java
@@ -0,0 +1,28 @@
+package com.example.valorantwallpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class VollbildActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vollbild);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+
+
+        Intent i = getIntent();
+
+        int position = i.getExtras().getInt("id");
+
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        imageView.setImageResource(imageAdapter.bilderArray[position]);
+    }
+}
