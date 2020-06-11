9e79c796224249a4bce45c15e2bbc2b49a53fa2f
GlitchSystemDE <GlitchSystem@outlook.de>
2020-06-11 12:19:12 +0200

    Hintergrund setzen

--- app/src/main/java/com/example/valorantwallpaper/VollbildActivity.java
+++ app/src/main/java/com/example/valorantwallpaper/VollbildActivity.java
@@ -2,15 +2,24 @@ package com.example.valorantwallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.io.IOException;
+import java.util.Objects;
 
 
 public class VollbildActivity extends AppCompatActivity {
@@ -30,16 +39,36 @@ public class VollbildActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         AdView.loadAd(adRequest);
 
+
         imageView = (ImageView) findViewById(R.id.image_view);
-
-
-
         Intent i = getIntent();
+        int position = Objects.requireNonNull(i.getExtras()).getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+        imageView.setImageResource(imageAdapter.bilderArray[position]);
 
-        int position = i.getExtras().getInt("id");
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                setWallpaper();
+            }
+        });
+    }
+
+    private void setWallpaper(){imageView = (ImageView) findViewById(R.id.image_view);
+        Intent i = getIntent();
+        int position = Objects.requireNonNull(i.getExtras()).getInt("id");
         ImageAdapter imageAdapter = new ImageAdapter(this);
 
-        imageView.setImageResource(imageAdapter.bilderArray[position]);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imageAdapter.bilderArray[position]);
+        WallpaperManager manager = WallpaperManager.getInstance(getApplicationContext());
+
+        try {
+            manager.setBitmap(bitmap);
+            Toast.makeText(this, "Wallpaper wurde gesetzt", Toast.LENGTH_SHORT).show();
+        } catch (IOException e){
+            Toast.makeText(this,"Error!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
